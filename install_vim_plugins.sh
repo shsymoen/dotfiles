a58e878357fd9f7dfa6765fe707cee0f9e0b6699
shsymoen <steffen.symoens@ugent.be>
2017-12-19 00:04:08 +0100

    update installation script for tmux plugins
    Signed-off-by: shsymoen <steffen.symoens@ugent.be>

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -5,3 +5,6 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone https://github.com/python-mode/python-mode.git ~/.vim/bundle/python-mode
 git clone https://github.com/powerline/powerline.git ~/.vim/bundle/powerline
+
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
