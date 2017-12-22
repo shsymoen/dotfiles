185d89e71441055e57383e1be18373723e49189f
mogeerts <moreno.geerts@ugent.be>
2017-12-22 15:23:22 +0100

    updated install script
    Signed-off-by: mogeerts <moreno.geerts@ugent.be>

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -3,5 +3,6 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
-git clone https://github.com/python-mode/python-mode.git ~/.vim/bundle/python-mode
+git clone --recursive https://github.com/python-mode/python-mode.git ~/.vim/bundle/python-mode
 git clone https://github.com/powerline/powerline.git ~/.vim/bundle/powerline
+git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
