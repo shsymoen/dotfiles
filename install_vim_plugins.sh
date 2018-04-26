a683994ed085d47ff7db69d9985cc78378d5b073
shsymoen <steffen.symoens@ugent.be>
2018-04-26 11:49:32 +0200

    Symbolic link creation contains full path now.
    Signed-off-by: shsymoen <steffen.symoens@ugent.be>

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -1,3 +1,4 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/.vim/
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -9,8 +10,8 @@ git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && cd 
 git clone https://github.com/jmcantrell/vim-virtualenv.git ~/.vim/bundle/vim-virtualenv
-ln -s .vimrc ~/.vimrc
+ln -s $DIR/.vimrc ~/.vimrc
 
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s .tmux.conf ~/.tmux.conf
+ln -s $DIR/.tmux.conf ~/.tmux.conf
