740ef3da55d97224fca1a8f81739f75af6fec761
shsymoen <steffen.symoens@ugent.be>
2018-12-06 19:30:12 +0100

    added git cloning for vim plugins: tagbar, fastfold, simpylfold, comfortable-motion, and ale
    Signed-off-by: shsymoen <steffen.symoens@ugent.be>

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -4,13 +4,17 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
-git clone --recursive https://github.com/python-mode/python-mode.git ~/.vim/bundle/python-mode
 git clone https://github.com/powerline/powerline.git ~/.vim/bundle/powerline
 git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 cd ~/.vim/bundle/YouCompleteMe ./install.py --clang-completer
 git clone https://github.com/jmcantrell/vim-virtualenv.git ~/.vim/bundle/vim-virtualenv
+git clone https://github.com/w0rp/ale.git ~/.vim/bundle/ale
+git clone https://github.com/tmhedberg/SimpylFold.git ~/.vim/bundle/SimpylFold
+git clone https://github.com/Konfekt/FastFold.git ~/.vim/bundle/FastFold
+git clone https://github.com/yuttie/comfortable-motion.vim.git ~/.vim/comfortable-motion.vim
+git clone git://github.com/majutsushi/tagbar ~/.vim/tagbar
 ln -s $DIR/.vimrc ~/.vimrc
 
 mkdir -p ~/.tmux/plugins
