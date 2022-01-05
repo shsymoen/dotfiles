e068b3584e9cef8104aec8121ed9470296b82093
shsymoen <symoens.steffen@gmail.com>
2022-01-05 21:39:22 +0100

    Updated installation of YouCompleteMe

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -8,7 +8,7 @@ git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone https://github.com/powerline/powerline.git ~/.vim/bundle/powerline
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
-cd ~/.vim/bundle/YouCompleteMe ./install.py --clang-completer
+cd ~/.vim/bundle/YouCompleteMe && python3 install.py --all
 git clone https://github.com/jmcantrell/vim-virtualenv.git ~/.vim/bundle/vim-virtualenv
 git clone https://github.com/dense-analysis/ale.git ~/.vim/bundle/ale
 git clone https://github.com/tmhedberg/SimpylFold.git ~/.vim/bundle/SimpylFold
