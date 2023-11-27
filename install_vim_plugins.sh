e2183d40fcb6014333d6cdb6c5b8199650a4fcd9
Steffen <steffen.juvenes@gmail.com>
2023-11-27 11:45:11 +0100

    Updated installation file with bard
    Signed-off-by: Steffen <steffen.juvenes@gmail.com>

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -1,35 +1,79 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir -p ~/.vim/
-mkdir -p ~/.vim/undodir
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-mkdir -p ~/.vim/colors
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
-git clone https://github.com/powerline/powerline.git ~/.vim/bundle/powerline
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
-cd ~/.vim/bundle/YouCompleteMe && python3 install.py --all
-git clone https://github.com/jmcantrell/vim-virtualenv.git ~/.vim/bundle/vim-virtualenv
-git clone https://github.com/dense-analysis/ale.git ~/.vim/bundle/ale
-git clone https://github.com/tmhedberg/SimpylFold.git ~/.vim/bundle/SimpylFold
-git clone https://github.com/Konfekt/FastFold.git ~/.vim/bundle/FastFold
-git clone https://github.com/sainnhe/everforest.git ~/.vim/bundle/everforest
-cp ~/.vim/bundle/everforest/colors/everforest.vim ~/.vim/colors/
-cp ~/.vim/bundle/everforest/autoload/everforest.vim ~/.vim/autoload/
-git clone https://github.com/yuttie/comfortable-motion.vim.git ~/.vim/bundle/comfortable-motion.vim
-git clone https://github.com/heavenshell/vim-pydocstring.git ~/.vim/bundle/vim-pydocstring.vim
-cd ~/.vim/bundle/vim-pydocstring.vim && make install
-git clone git://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar
-git clone https://github.com/puremourning/vimspector ~/.vim/pack/vimspector/opt/vimspector
-ln -s $DIR/.vimrc ~/.vimrc
+#!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Create necessary Vim directories
+mkdir -p ~/.vim/{undodir,autoload,bundle,color}
+
+# Define the list of packages to install with full repo paths
+packages=(
+  "https://github.com/tpo/pathogen.vim.git"
+  "https://github.com/scrooloose/nerdtree.git"
+  "https://github.com/kien/ctrlp.vim.git"
+  "https://github.com/powerline/powerline.git"
+  "https://github.com/Valloric/YouCompleteMe.git"
+  "https://github.com/jmcantrell/vim-virtualenv.git"
+  "https://github.com/dense-analysis/ale.git"
+  "https://github.com/tmhedberg/SimpylFold.git"
+  "https://github.com/Konfekt/FastFold.git"
+  "https://github.com/sainnhe/everforest.git"
+  "https://github.com/yuttie/comfortable-motion.vim.git"
+  "https://github.com/heavenshell/vim-pydocstring.vim.git"
+)
+
+# Install Vim if it's not already installed
+if ! command -v vim &> /dev/null; then
+  echo "Vim is not installed. Installing Vim..."
+  sudo pacman -S vim
+fi
+
+# Install each package from the list
+for package_url in "${packages[@]}"; do
+  package_name=$(basename "${package_url}")
+  # Extract the package name from the URL
+
+  if [ ! -d ~/.vim/bundle/$package_name ]; then
+    echo "Installing $package_name..."
+    git clone ${package_url} ~/.vim/bundle/$package_name
+  fi
+done
+
+# Install everforest colors and autoload files
+if [ -d ~/.vim/bundle/everforest ]; then
+  echo "Copying everforest colors and autoload files..."
+  cp ~/.vim/bundle/everforest/colors/everforest.vim ~/.vim/colors/
+  cp ~/.vim/bundle/everforest/autoload/everforest.vim ~/.vim/autoload/
+fi
+
+# Update and install YouCompleteMe
+if [ -d ~/.vim/bundle/YouCompleteMe ]; then
+  cd ~/.vim/bundle/YouCompleteMe
+  echo "Updating and installing YouCompleteMe..."
+  git submodule update --init --recursive
+  python3 install.py --all
+fi
+
+# Install vim-pydocstring
+if [ -d ~/.vim/bundle/vim-pydocstring.vim ]; then
+  cd ~/.vim/bundle/vim-pydocstring.vim
+  echo "Installing vim-pydocstring..."
+  make install
+fi
+
+# Create link to vimrc
+ln -s "$DIR/.vimrc" ~/.vimrc
+
+# Install additional dependencies
+echo "Installing additional dependencies..."
 conda install black
 pip install doq
 
+# Install Tmux plugins
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s $DIR/.tmux.conf ~/.tmux.conf
+ln -s "$DIR/.tmux.conf" ~/.tmux.conf
 
+# Replace xfce4-keyboard-shortcuts.xml
+echo "Replacing xfce4-keyboard-shortcuts.xml..."
 rm ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 ln -s $DIR/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
